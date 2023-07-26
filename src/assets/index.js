a35ad168628543d67ba28e236f80e1df170de02c
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-07-26 09:06:57 -0400

    image rename and imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,24 @@
 // * This file is used to import all the images in the assets folder
 // * You simply copy/paste or drag+drop the images into the assets folder and follow the same format as below
 import BusinessLogo from "./BusinessLogo.png";
-import lawn1 from "./lawn1.jpg";
-import lawn2 from "./lawn2.jpg";
-import lawn3 from "./lawn3.jpg";
-import lawn4 from "./lawn4.jpg";
-import lawn5 from "./lawn5.jpg";
-import lawn6 from "./lawn6.jpg";
-import lawn7webp from "./lawn7.webp";
+import cornerYard from "./cornerYard.jpg";
+import cornerYard2 from "./cornerYard2.jpg";
+import cornerYard3 from "./cornerYard3.jpg";
+import fencedYard from "./fencedYard.jpg";
+import sidewalk from "./sidewalk.jpg";
+import sidewalk2 from "./sidewalk2.jpg";
+import yard1 from "./yard1.jpg";
+import yard2 from "./yard2.jpg";
 
 // * Make sure to export any new images you add to the assets folder
-export { BusinessLogo, lawn1, lawn2, lawn3, lawn4, lawn5, lawn6, lawn7webp };
+export {
+  BusinessLogo,
+  cornerYard,
+  cornerYard2,
+  cornerYard3,
+  fencedYard,
+  sidewalk,
+  sidewalk2,
+  yard1,
+  yard2,
+};
